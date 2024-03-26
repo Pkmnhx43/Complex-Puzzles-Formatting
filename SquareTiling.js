3df318ede8297c6d0f4e9c1ea9bc619e21d9ec3a
Trey <43812721+Pkmnhx43@users.noreply.github.com>
2024-03-26 18:54:46 -0400

    Attempt to change piece sizes

--- SquareTiling.js
+++ SquareTiling.js
@@ -69,7 +69,7 @@ class SquareK5Piece {
     }
 
     static getSizes(canvas) {
-        const gridX = 1<<3;
+        const gridX = 1<<2;
         const size = canvas.height / (3 * gridX);
         const spacingX = 3 * size;
         const spacingY = 3 * size;
